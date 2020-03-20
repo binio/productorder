4bca5b2918b32de5ac767e39d2481e8d10244649
binio <tbiniecki@gmail.com>
2020-03-20 14:41:06 +0000

    remove sample data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA testdb;
+
+--DELETE FROM product where 1=1;
+--INSERT INTO product (product_id, product_created_date, product_deleted, product_name, product_price, product_sku) VALUES ('1', '2020-03-19 17:01:46', true, 'Product One', 33.20, '5000989875');
+--INSERT INTO product (product_id, product_created_date, product_deleted, product_name, product_price, product_sku) VALUES ('2', '2020-03-15 12:02:46', true, 'Product Two Thomas', 12.16, '5000988765');
